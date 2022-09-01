876412d123f65d3d04c32468b64aaeae7a50a2b2
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-09-01 18:37:08 -0300

    feat: added DbContext and Database Configuration

---
+++ Models/LojaContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CadastroClientes.Models
+{
+    public class LojaContext : DbContext
+    {
+        public LojaContext() : base("DbLoja") { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Consultor> Consultores { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+    }
+}
