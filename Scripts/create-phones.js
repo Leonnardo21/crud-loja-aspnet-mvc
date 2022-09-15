d8b75b3c0ec3878d00d13291c5316e21d8b399ca
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-09-15 16:38:30 -0300

    feat: phone input function remove dynamically

--- Scripts/create-phones.js
+++ Scripts/create-phones.js
@@ -5,20 +5,20 @@
         var blocoTelefone = '<div class="row">' +
             '    <div class="col-md-2">' +
             '        <input type="number" name="Telefones[' + qtdPhones + '].DDD" maxlength = "2" placeholder = "DDD" class="form-control txt-ddd" /> ' +
-        '    </div>' +
+            '    </div>' +
             '    <div class="col-md-6">' +
             '        <input type="text" name="Telefones[' + qtdPhones + '].Numero" placeholder = "Numero" class="form-control txt-numero" /> ' +
-        '    </div>' +
+            '    </div>' +
             '    <div class="col-md-3">' +
             '        <select name="Telefones[' + qtdPhones + '].Tipo" class="form-control sel-tipo" > ' +
-        '            <option value="0">Residencial</option>' +
+            '            <option value="0">Residencial</option>' +
             '            <option value="1">Comercial</option>' +
             '            <option value="2">Celular</option>' +
             '            <option value="3">Recado</option>' +
             '        </select>' +
             '    </div>' +
             '    <div class="col-md-1">' +
-            '        <button class="btn btn-danger btn-remover-telefone">' +
+            '        <button class="btn btn-danger btn-remove-phones">' +
             '            <span class="glyphicon glyphicon-trash"></span>' +
             '        </button>' +
             '    </div>' +
@@ -26,5 +26,11 @@
 
         $("#div-phones").append(blocoTelefone);
         qtdPhones++;
+    });
+
+    $("#div-phones").on("click", ".btn-remove-phones", function (e) {
+        e.preventDefault();
+
+        $(this).parent().parent().remove();
     });
 });
